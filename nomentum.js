a677ed3463a12b8eb1c91fb9dc6153b8dde9120f
vincens2005 <bokheadpictures@gmail.com>
2017-10-30 19:32:26 -0700

    Update nomentum.js

--- nomentum.js
+++ nomentum.js
@@ -7,14 +7,7 @@ function setGreet(){
        
 }
 }
-chrome.tabs.executeScript(tab.id, {
-    code : 'var script=document.createElement(\'script\');' +
-    'script.onload=function() { /*do something in the page after the script was loaded*/ };' +
-    'script.src=\'https://quotesondesign.com/api/3.0/api-3.0.js\';' +
-    'document.body.appendChild(script);'
-}, function (returnedValue) {
-    // do something in the extension context after the code was executed
-});
+
  /*var randomNumber = Math.floor(Math.random() * 36);
 */
 // Docs at http://simpleweatherjs.com
